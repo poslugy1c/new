a94ed52a1148f3f90fc0700d6345bbd455f52ea0
Юрий Хмелевский <yuri.khmelevski@symbol.ua>
2021-08-02 16:35:44 +0300

    renew dz 2

--- dz1_2.php
+++ dz1_2.php
@@ -3,14 +3,29 @@
 echo '2. Написать функцию которая выводит первые N чисел фибоначчи';
 echo '<br> <br>';
 
-function fibo($i)
-{
-    if ($i == 0) return 0;
-    if ($i == 1 || $i == 2) {
-        return 1;
-    } else {
-        return fibo($i - 1) + fibo($i - 2);
-    }
+// function fibo($i)
+// {
+//     if ($i == 0) echo 0;
+//     if ($i == 1 || $i == 2) {
+//         echo 1;
+//     } else {
+//         echo fibo($i - 1) + fibo($i - 2);
+//     }
+// }
+
+
+function fibo($n){
+    $firstNum  = 0;
+    $SecondNum = 1;
+    $numFibo = 0;
+
+    while ($n > 0) {
+        $numFibo =$firstNum +$SecondNum;
+        echo $numFibo . '<br>';
+        $firstNum = $SecondNum;
+        $SecondNum = $numFibo;       
+        $n--;
+    } 
 }
 
-fibo(5);
+fibo(12);
