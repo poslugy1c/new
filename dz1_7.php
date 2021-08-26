73d10dfcfeb4e21b94a237e092cc9d0d173396d5
Юрий Хмелевский <yuri.khmelevski@symbol.ua>
2021-08-26 13:19:45 +0300

    added  recursion 4  for  dz 1.7

--- dz1_7.php
+++ dz1_7.php
@@ -69,3 +69,105 @@ function myPow($n, $m)
 }
 
 echo myPow(5, -2);
+
+echo '<br>';
+echo '<br>';
+
+echo '4. Рекурсия. Написать функцию которая вычисляет входит ли IP-адрес в диапазон указанных IP-адресов. Вычислить для версии ipv4.';
+echo '<br>';
+
+function inDiapazon2($ip, $beginDiap, $endDiap)
+{
+    $arrIp = array();
+    $triada ='';
+    $numTriad =0;
+
+    for ($i = 0; $i < strlen($ip); $i++){
+
+        if($ip[$i] == '.'){
+            $arrIp[] = (int) $triada;
+            $numTriad++;
+            // echo '<br> ' . (int) $triada .' / '. $numTriad;
+
+            $bTriada ='';
+            $numBeginTriad =0;
+            $eTriada ='';
+            $numEndTriad =0;
+
+            for ($j = 0; $j < strlen($beginDiap); $j++){               
+                if($beginDiap[$j] == '.'){
+                    $numBeginTriad++;
+
+                    if( ((int) $triada < (int)$bTriada) && $numBeginTriad == $numTriad){
+                        return $ip .' not in diapazon';
+                    } 
+
+                    $bTriada = '';
+                } else {
+                    $bTriada = $bTriada . $beginDiap[$j];
+                }
+            }   
+
+            for ($x = 0; $x < strlen($endDiap); $x++){
+                
+                if($endDiap[$x] == '.'){
+                    $numEndTriad++;
+
+                    if( ((int) $triada > (int)$eTriada) && $numEndTriad == $numTriad){
+                        return $ip .' not in diapazon';
+                    } 
+
+                    $eTriada = '';
+                } else {
+                    $eTriada = $eTriada . $endDiap[$x];
+                }
+            }    
+
+            $triada = '';
+        }else{
+            $triada = $triada . $ip[$i];
+        }
+    }
+
+    $arrIp[] = (int) $triada;
+    $numBeginTriad++;
+    $numEndTriad++;
+    $numTriad++;
+
+    // echo '<br> ' . (int) $triada .' / '. $numTriad;
+    // echo '********  ' . (int) $bTriada .' / '. $numBeginTriad;
+
+    if( ((int) $triada < (int)$bTriada) && $numBeginTriad == $numTriad){
+        return $ip .' not in diapazon';
+    } 
+    if( ((int) $triada > (int)$eTriada) && $numEndTriad == $numTriad){
+        return $ip .' not in diapazon';
+    } 
+
+    return $ip .' in Diapazon';
+}
+
+function inDiapazon($ip, $beginDiap, $endDiap, $triada =0)
+{
+    // return $ip .' not in diapazon';
+    $arrIp = explode(".", $ip);
+    $arrBeginDiap = explode(".", $beginDiap);
+    $arrEndDiap = explode(".", $endDiap);
+
+    if($triada >count($arrIp)-1){
+        return $ip .' in diapazon';
+    }elseif(($arrIp[$triada] < $arrBeginDiap[$triada]) || ($arrIp[$triada] > $arrEndDiap[$triada])){
+
+        // echo '<br> ';
+        // echo $triada;
+        // echo '<br> ';
+
+        return $ip .' not in diapazon';
+    };
+
+    return inDiapazon($ip, $beginDiap, $endDiap, ++$triada);
+}
+
+$res =inDiapazon('192.168.1.48', '192.168.1.44', '192.168.1.255');
+
+echo $res;
