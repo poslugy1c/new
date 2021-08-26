c51bdbca86f5306c4c6c029ca4f97e631f5ffda2
Юрий Хмелевский <yuri.khmelevski@symbol.ua>
2021-08-26 14:21:35 +0300

    added  recursion 5  for  dz 1.7

--- dz1_7.php
+++ dz1_7.php
@@ -76,77 +76,6 @@ echo '<br>';
 echo '4. Рекурсия. Написать функцию которая вычисляет входит ли IP-адрес в диапазон указанных IP-адресов. Вычислить для версии ipv4.';
 echo '<br>';
 
-function inDiapazon2($ip, $beginDiap, $endDiap)
-{
-    $arrIp = array();
-    $triada ='';
-    $numTriad =0;
-
-    for ($i = 0; $i < strlen($ip); $i++){
-
-        if($ip[$i] == '.'){
-            $arrIp[] = (int) $triada;
-            $numTriad++;
-            // echo '<br> ' . (int) $triada .' / '. $numTriad;
-
-            $bTriada ='';
-            $numBeginTriad =0;
-            $eTriada ='';
-            $numEndTriad =0;
-
-            for ($j = 0; $j < strlen($beginDiap); $j++){               
-                if($beginDiap[$j] == '.'){
-                    $numBeginTriad++;
-
-                    if( ((int) $triada < (int)$bTriada) && $numBeginTriad == $numTriad){
-                        return $ip .' not in diapazon';
-                    } 
-
-                    $bTriada = '';
-                } else {
-                    $bTriada = $bTriada . $beginDiap[$j];
-                }
-            }   
-
-            for ($x = 0; $x < strlen($endDiap); $x++){
-                
-                if($endDiap[$x] == '.'){
-                    $numEndTriad++;
-
-                    if( ((int) $triada > (int)$eTriada) && $numEndTriad == $numTriad){
-                        return $ip .' not in diapazon';
-                    } 
-
-                    $eTriada = '';
-                } else {
-                    $eTriada = $eTriada . $endDiap[$x];
-                }
-            }    
-
-            $triada = '';
-        }else{
-            $triada = $triada . $ip[$i];
-        }
-    }
-
-    $arrIp[] = (int) $triada;
-    $numBeginTriad++;
-    $numEndTriad++;
-    $numTriad++;
-
-    // echo '<br> ' . (int) $triada .' / '. $numTriad;
-    // echo '********  ' . (int) $bTriada .' / '. $numBeginTriad;
-
-    if( ((int) $triada < (int)$bTriada) && $numBeginTriad == $numTriad){
-        return $ip .' not in diapazon';
-    } 
-    if( ((int) $triada > (int)$eTriada) && $numEndTriad == $numTriad){
-        return $ip .' not in diapazon';
-    } 
-
-    return $ip .' in Diapazon';
-}
-
 function inDiapazon($ip, $beginDiap, $endDiap, $triada =0)
 {
     // return $ip .' not in diapazon';
@@ -157,11 +86,9 @@ function inDiapazon($ip, $beginDiap, $endDiap, $triada =0)
     if($triada >count($arrIp)-1){
         return $ip .' in diapazon';
     }elseif(($arrIp[$triada] < $arrBeginDiap[$triada]) || ($arrIp[$triada] > $arrEndDiap[$triada])){
-
         // echo '<br> ';
         // echo $triada;
         // echo '<br> ';
-
         return $ip .' not in diapazon';
     };
 
@@ -171,3 +98,52 @@ function inDiapazon($ip, $beginDiap, $endDiap, $triada =0)
 $res =inDiapazon('192.168.1.48', '192.168.1.44', '192.168.1.255');
 
 echo $res;
+
+echo '<br>';
+echo '<br>';
+
+echo '5. Рекурсия. Для одномерного массива
+<br> - Подсчитать процентное соотношение положительных/отрицательных/нулевых/простых чисел
+<br> - Отсортировать элементы по возрастанию/убыванию';
+echo '<br>';
+echo '<br>';
+
+function PercentRatio($arr, $n =0, $numNegative =0, $numPositive =0, $num0 =0, $simple =0){
+
+    if($n >count($arr)-1){
+        //возвращаю результат
+        echo 'Процент отрицательных в массиве: ' . ($numNegative * 100 / count($arr)) .'% <br>';
+        echo 'Процент положительных в массиве: ' . ($numPositive * 100 / count($arr)) .'% <br>';
+        echo 'Процент нулей в массиве: ' . ($num0 * 100 / count($arr)) . '% <br>';
+        echo 'Процент простых чисел: ' . ($simple * 100 / count($arr)) . '% <br>';
+        return 0;
+    };
+
+    if ($arr[$n] < 0){
+        $numNegative++;
+    }else if($arr[$n] > 0){
+        $numPositive++;
+    }else{
+        $num0++;
+    };
+
+    $isSimlple =true;
+    for($j = 2; $j < $arr[$n]; $j++){
+        if ($arr[$n] % $j == 0) 
+        {
+            $isSimlple =false;
+            break;
+        } 
+    }
+
+    if($isSimlple && $arr[$n] != 0) {
+        $simple++;
+    };
+
+    return PercentRatio($arr, ++$n, $numNegative, $numPositive,$num0,$simple);   
+}; 
+
+$newArr = array(5, 7, 0 , 444, 17, 8, 4, 0, 8, -29, 0, 8, 4, 188, 11, 5); 
+
+PercentRatio($newArr);
+
